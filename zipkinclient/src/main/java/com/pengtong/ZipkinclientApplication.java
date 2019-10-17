a8431fe374550b74374a7d69f58f1aa4b2c2c206
1239008581 <1239008581@qq.com>
2019-10-17 08:42:16 +0800

    aaa

---
+++ zipkinclient/src/main/java/com/pengtong/ZipkinclientApplication.java
@@ -0,0 +1,13 @@
+package com.pengtong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinclientApplication.class, args);
+    }
+
+}
